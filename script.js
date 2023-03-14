b01e04bf508669327524a038a25f097aa87eceb2
Sergio Luiz Falcetti <falcettijr@gmail.com>
2023-03-14 17:01:33 -0300

    Copiando de volta os arquivos antigos

--- script.js
+++ script.js
@@ -1,21 +1,47 @@
 var btnsDecrementar = document.querySelectorAll(".btn-decrementar")
 var btnsIncrementar = document.querySelectorAll(".btn-incrementar")
 
+for(let btn of btnsIncrementar) {
+    btn.onclick = function(){
+
+        //Obter Elementos
+        let tr = this.closest("tr");
+        let tdValorUnitario = tr.querySelector("td.valorUnitario");
+        let tdQuantidade = tr.querySelector("td.quantidade");
+        let tdValorTotalProduto = tr.querySelector(".valorTotalProduto");
+
+        //Calcular Quantidades e Valores
+        let novaQuantidade = tdQuantidade.getAttribute("data-quantidade") + 1;
+        let novoValorTotal = novaQuantidade * tdValorUnitario.getAttribute("data-valorunitario");   
+        
+        //Alterar Dados nos elementos
+        tdQuantidade.setAttribute("data-quantidade", novaQuantidade);
+        tdValorTotalProduto.setAttribute("data-valortotalproduto", novoValorTotal);
+
+        //Alterar informações na tabela
+        tdQuantidade.querySelector("span").innerHTML = novaQuantidade;
+        tdValorTotalProduto.textContent = "R$ " + novoValorTotal;
+
+        console.log(novaQuantidade);
+        console.log(novoValorTotal);
+    }
+}
+
 for(let btn of btnsDecrementar) {
     btn.onclick = function() {
 
         //Obter Elementos
         let tr = this.closest("tr");
-        let tdvalorUnitario = tr.querySelector("td.valorUnitario");
+        let tdValorUnitario = tr.querySelector("td.valorUnitario");
         let tdQuantidade = tr.querySelector("td.quantidade");
         let tdValorTotalProduto = tr.querySelector(".valorTotalProduto");
 
         //Calcular Quantidades e Valores
         let novaQuantidade = tdQuantidade.getAttribute("data-quantidade") - 1;
-        if (novaQuantidade < 1) {
-            novaQuantidade = 1;
-        }
-        let novoValorTotal = novaQuantidade * tdvalorUnitario.getAttribute("data-valorunitario");
+            if (novaQuantidade < 1) {
+                    novaQuantidade = 1;
+                }
+        let novoValorTotal = novaQuantidade * tdValorUnitario.getAttribute("data-valorunitario");
 
         //Alterar Dados nos elementos
         tdQuantidade.setAttribute("data-quantidade", novaQuantidade);
@@ -23,15 +49,9 @@ for(let btn of btnsDecrementar) {
 
         //Alterar informações na tabela
         tdQuantidade.querySelector("span").innerHTML = novaQuantidade;
-        tdValorTotalProduto.textContent = ("R$ " + novoValorTotal).replace(".", ",");
+        tdValorTotalProduto.textContent = "R$ " + novoValorTotal;
 
         console.log(novaQuantidade);
         console.log(novoValorTotal);
     }
 }
-
-// for(let btn of btnsIncrementar) {
-//     btn.onclick = function(){
-//         alert("Aumentou")
-//     }
-// }
